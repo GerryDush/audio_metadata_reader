823b9556929a06ae160150f17c720701ac17ba90
GerryDush <liu_yupei@foxmail.com>
2025-09-21 02:42:11 +0800

    fix: mp3 ttml read

--- example/main.dart
+++ example/main.dart
@@ -4,12 +4,12 @@ import 'dart:typed_data';
 import 'package:audio_metadata_reader/audio_metadata_reader.dart';
 
 void main() {
-  final track = File("9357CBDA-A004-4836-82B1-ABF03ADBC656.flac");
+  final track = File("Carpenters - Yesterday Once More.mp3");
 
   // Returns a condensate
   // Getting the image of a track can be heavy and slow the reading
   AudioMetadata metadata = readMetadata(track, getImage: true);
-
+// print(metadata);
   // print(metadata);
 
 }
